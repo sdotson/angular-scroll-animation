da4e830e324d69af5d07d07691abb4beae6d9e42
Stu <stuart@blueprintinteractive.com>
2015-09-12 12:32:06 -0400

    revise and simplify app, initial experiment with scroll directive

--- app/app.js
+++ app/app.js
@@ -1,9 +1,27 @@
-angular.module('countriesApp',['countriesList','countryDetail','home','countries','ngAnimate'])
-	.run(function($rootScope, countriesService) {
-		$rootScope.$on('$routeChangeStart', function() {
-      $rootScope.isLoading = true;
+angular.module('myApp',[])
+
+    .controller('scrollController', scrollController)
+
+    .directive('scroll', function($window) {
+        return function(scope, el, attrs) {
+
+            angular.element($window).bind("scroll", function () {
+                var offset = $window.pageYOffset;
+                 if (offset >= 50) {
+                     scope.boolChangeClass = true;
+                     console.log('scrolling');
+                 } else {
+                     scope.boolChangeClass = false;
+                 }
+                scope.$apply();
+            });
+        };
     });
-    $rootScope.$on('$routeChangeSuccess', function() {
-      $rootScope.isLoading = false;
-    });
-	});
+
+
+scrollController.$inject = [];
+
+function scrollController() {
+
+}
+
