fa4357e1ecdbf1ba983b54db4c5b44e6fe3ba035
Stu <stuart@blueprintinteractive.com>
2015-09-13 17:03:23 -0400

    fix build bugs

--- app/app.js
+++ app/app.js
@@ -2,48 +2,11 @@
     'use strict';
 
     angular.module('myApp',['ngAnimate', 'ngRoute'])
-
         .controller('scrollController', scrollController)
-
         .config(config)
-
-        .directive('scroll', function($window) {
-            return function(scope, el, attrs) {
-
-                var sections = el.find('div');
-
-                angular.element($window).bind("scroll", function () {
-
-                    var offset = $window.pageYOffset,
-                        windowTop = $window.pageYOffset,
-                        windowHeight = $window.outerHeight;
-
-                    for (var i = 0, sectionsLength = sections.length; i < sectionsLength; i++) {
-                       var sectionTop = angular.element(sections[i]).prop('offsetTop'),
-                           sectionHeight = angular.element(sections[i]).prop('offsetHeight'),
-                           sectionMidpoint = sectionTop + sectionHeight/2,
-                           sectionBottom = sectionTop + sectionHeight,
-                           windowMidpoint = windowTop + windowHeight/2,
-                           windowBottom = windowTop + windowHeight;
-
-                       if (windowTop > sectionTop && windowTop < sectionBottom) {
-                            var index = i,
-                                change = (windowTop - sectionTop)/(sectionHeight);
-
-                                scope.slide = i;
-                                scope.change = change;
-
-                        };
-                    };
-                     
-                    scope.$apply();
-                });
-            };
-        });
-
+        .directive('scroll', scrollDirective);
 
     scrollController.$inject = ['$scope', '$location'];
-
     function scrollController($scope, $location) {
         $scope.slide = 0;
 
@@ -71,6 +34,42 @@
             redirectTo: '/'
         });
     }
+
+    scrollDirective.$inject = ['$window'];
+    function scrollDirective($window) {
+        return function(scope, el, attrs) {
+
+            var sections = el.find('div');
+
+            angular.element($window).bind("scroll", function () {
+
+                var offset = $window.pageYOffset,
+                    windowTop = $window.pageYOffset,
+                    windowHeight = $window.outerHeight;
+
+                for (var i = 0, sectionsLength = sections.length; i < sectionsLength; i++) {
+                   var sectionTop = angular.element(sections[i]).prop('offsetTop'),
+                       sectionHeight = angular.element(sections[i]).prop('offsetHeight'),
+                       sectionMidpoint = sectionTop + sectionHeight/2,
+                       sectionBottom = sectionTop + sectionHeight,
+                       windowMidpoint = windowTop + windowHeight/2,
+                       windowBottom = windowTop + windowHeight;
+
+                   if (windowTop > sectionTop && windowTop < sectionBottom) {
+                        var index = i,
+                            change = (windowTop - sectionTop)/(sectionHeight);
+
+                            scope.slide = i;
+                            scope.change = change;
+
+                    };
+                };
+                 
+                scope.$apply();
+            });
+        };
+    };
+
 
 })();
 
